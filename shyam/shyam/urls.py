fa22bed1b81093a68ab0efa0a73fe944eca11c97
SridharShyam <shyamsridhar935@gmail.com>
2023-11-24 13:21:15 +0530

    Success

---
+++ shyam/shyam/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('areaofsquareprism/',views.squarearea,name="areaofsquareprism"),
+    path('',views.squarearea,name="areaofsquareprism")
+]
